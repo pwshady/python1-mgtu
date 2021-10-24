92d2be1ea909e7ce07ba71cd5cde49647d372e7f
pwshady <pwshady@gmail.com>
2021-10-24 16:54:34 +0300

    lab-1

--- labs/lab1.py
+++ labs/lab1.py
@@ -1,8 +1,37 @@
 import random
-num1 = 0
-num2 = 0
-num3 = 0
-for i in range(0,100):
-    prize = random.randrange(0, 3)
-    if prize == 1:
-        if
+
+choices = ['empty', 'empty', 'prize']
+
+N = 100000
+
+# проверяем вариант с неизменным выбором
+win_count = 0
+for _ in range(N):
+    # в начале каждой попытки случайно перемешаем массив
+    random.shuffle(choices)
+    result = choices[random.randrange(len(choices))]
+    if result == 'prize':
+        win_count += 1
+# вероятность выиграть - частота выигранных опытов
+print(win_count/N)
+
+# проверяем вариант с изменением выбора
+win_count = 0
+for _ in range(N):
+    # в начале каждой попытки случайно перемешаем массив
+    random.shuffle(choices)
+    # первый выбор
+    first_choice = random.randrange(len(choices))
+    # ведущий открывает дверь с empty
+    for i in range(len(choices)):
+        if i != first_choice and choices[i] == 'empty':
+            host_choice = i
+            break
+    # делаем второй выбор, меняя первое решение
+    for i in range(len(choices)):
+        if i != first_choice and i != host_choice:
+            result = choices[i]
+    if result == 'prize':
+        win_count += 1
+# вероятность выиграть - частота выигранных опытов
+print(win_count/N)
